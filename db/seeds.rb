bbaac8e41dde72f8b9d0b2aedec2cb838863a74b
codelitt <littlewoodcody@gmail.com>
2013-10-05 17:05:31 -0300

    added seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 #
-schools = School.create([{ name
+schools = School.create([{ name: 'cool school', zip: '10014'}])
