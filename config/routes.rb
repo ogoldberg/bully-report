83614665e477cfd6392edaed29c75e2786a15a04
codelitt <littlewoodcody@gmail.com>
2013-10-05 04:11:21 -0300

    added controller and views for schools

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 BullyReport::Application.routes.draw do
+  get "schools/index"
+  get "schools/show"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
