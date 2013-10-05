30dcdd07595bc8d085138c291f365a0b8f72a11d
clickslide <hello@clickslide.co>
2013-10-05 18:47:44 -0400

    fixing search and adding reports controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,5 @@
 class SearchController < ApplicationController
   def index
-    @schools = School.where('zip = ? OR address ilike ? OR name ilike ? OR (lat = ? AND lng = ?)', params[:zip], params[:address], params[:name], params[:lat], params[:lng])  
+    @schools = School.where('zip = ? OR address ~* ? OR name ~* ? OR (lat = ? AND lng = ?)', params[:zip], params[:address], params[:name], params[:lat], params[:lng])  
   end
 end
