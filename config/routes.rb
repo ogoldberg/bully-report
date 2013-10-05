36f2235ed0edb0af8899f0d627f0675afb31b7ab
codelitt <littlewoodcody@gmail.com>
2013-10-05 04:43:23 -0300

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 BullyReport::Application.routes.draw do
-  get "schools/index"
-  get "schools/show"
+  root :to => 'schools#index' 
+  resources :schools, :only => [:index, :show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
