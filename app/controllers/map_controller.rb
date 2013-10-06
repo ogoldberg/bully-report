687f9f7022a79f78769bcff2966ca1c6d4723402
Tony Holdstock-Brown <tonyhb@gmail.com>
2013-10-06 05:40:50 -0400

    Add heatmaps

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -3,4 +3,20 @@ class MapController < ApplicationController
   def index
   end
 
+  def search
+    lat = params[:latitude].to_f
+    lng = params[:longitude].to_f
+
+    if lat == 0 or lng == 0
+      return redirect_to "/map"
+    end
+
+    schools = School.within(4, :origin => [lat, lng]).load()
+
+    respond_to do |format|
+      format.html { render :json => schools }
+      format.json { render :json => schools }
+    end
+  end
+
 end
